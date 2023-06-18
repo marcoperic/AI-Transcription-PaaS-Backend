a27b9bafc697322bf1a849601496547ee33356e7
Marco Peric <mperic@knights.ucf.edu>
2023-06-17 22:13:51 -0400

    dispatch check file ext

--- backend/master/master.py
+++ backend/master/master.py
@@ -2,6 +2,7 @@ from load_balancer import LoadBalancer
 from threading import Thread
 from pathlib import Path
 from time import sleep
+import ffmpeg
 import json
 import os
 
@@ -33,15 +34,21 @@ class Master():
     def enqueue(self, fileName, ext):
         file = MEDIA_DIR + fileName + '.' + ext
         instructions = MEDIA_DIR + '.json'
-        instructions = json.load(instructions)
+        # instructions = json.load(instructions)
         self.file_queue.append((file, instructions))
+        print('successfully enqueued ' + fileName)
 
     '''
     Read JSON instructions and package file before sending to a node determined by the load balancer
     '''
     def dispatch(self):
-        pass
-
+        while True:
+            if (len(self.file_queue) > 0):
+                file, instructions = self.file_queue.pop(0)
+                file = r"{}".format(file)
+                extension = str('' + file).split('.')[1]
+                # print(file)
+                print(ffmpeg.probe(file))                
     '''
     Handle incoming data from worker node and send back to the frontend. Delete the files in directory.
     '''
@@ -66,3 +73,4 @@ class Master():
 if __name__ == "__main__":
     m = Master()
     lb = LoadBalancer(m)
+    m.enqueue('kitten', 'jfif')
