f3c9ae2def6fd3c3cab58c02199b043e1ffdfbc0
Marco Peric <mperic@knights.ucf.edu>
2023-06-20 20:43:22 -0400

    translation and transcription working on backend

--- backend/node/worker.py
+++ backend/node/worker.py
@@ -3,6 +3,20 @@ class Worker():
     def __init__(self) -> None:
         pass
 
+# from subsai import SubsAI
+# from subsai import Tools
+# import pysubs2
+
+# print('test')
+# file = 'test.mp3'
+# subs = SubsAI()
+# model = subs.create_model('ggerganov/whisper.cpp', {'model_type':'tiny'})
+# output = subs.transcribe(file, model)
+# output.save('goomba2.srt')
+
+# subtitles = pysubs2.load('goomba2.srt')
+# translated_subs = Tools.translate(subtitles, source_language='English', target_language='Croatian', model='facebook/m2m100_1.2B')
+# translated_subs.save('translated.srt')
 
 '''
 # worker node sample
--- docs/jawn.py
+++
@@ -1,6 +0,0 @@
-import json
-
-fp = open('docs/sample_instructions.json')
-x = json.load(fp)
-x['job']['userID'] = '1231iu23u1231nksajnasd98asnd9'
-print(x)
