a5d18f3eecf46b1fc0461cb6fa0a742f2b679ed0
Marco Peric <mperic@knights.ucf.edu>
2023-06-24 23:54:15 -0400

    sample api endpoints made

--- backend/master/api_controller.js
+++ backend/master/api_controller.js
@@ -1,12 +1,23 @@
 const express = require('express')
+const fs = require('fs/promises')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.send('' + Math.random());
 });
 
 app.get('/goomba', (req, res) => {
     res.send('GOOMBA');
 });
+
+app.get('/upload_media', (req, res) => {
+
+    fs.readFile('../../testing/sample_instructions_package.json', 'utf8').then((data) =>
+    {
+        res.json(JSON.parse(data))
+    }).catch((err) => {
+        console.log(err)
+    });
+})
 
 app.listen(3000, () => console.log("Server is listening on port 3000."))
