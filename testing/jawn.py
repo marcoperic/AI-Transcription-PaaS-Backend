4b62301715f5484096d8024c6eb73022e37cca10
Marco Peric <mperic@knights.ucf.edu>
2023-06-30 15:31:28 -0400

    more testing

--- testing/jawn.py
+++ testing/jawn.py
@@ -1,23 +1,25 @@
 import json
 import base64
+from subsai import SubsAI
+from subsai import Tools
+import pysubs2
+from datetime import datetime
+from pywhispercpp.model import Model
 
 # fp = open('docs/sample_instructions.json')
 # x = json.load(fp)
 # x['job']['userID'] = '1231iu23u1231nksajnasd98asnd9'
 # print(x)
 
-# from subsai import SubsAI
-# from subsai import Tools
-# import pysubs2
-# from datetime import datetime
 
 # print(datetime.now())
-# file = 'test.mp3'
-# subs = SubsAI()
-# model = subs.create_model('openai/whisper', {'model_type':'base', 'device':'cpu', 'language': 'nl'})
-# output = subs.transcribe(file, model)
-# output.save('multiple_language_transcription.srt')
-# print(datetime.now())
+file = 'test.mp3'
+subs = SubsAI()
+print(subs.available_models())
+model = subs.create_model('guillaumekln/faster-whisper', {'model_type':'tiny', 'device':'cpu', 'language': 'hr'})
+output = subs.transcribe(file, model)
+output.save('multiple_language_transcription.srt')
+print(datetime.now())
 
 
 # subtitles = pysubs2.load('goomba2.srt')
